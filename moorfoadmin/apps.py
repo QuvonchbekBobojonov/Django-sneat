d986a1b72ebe43470956faf71413cc35e855230f
Quvonchbek Bobojonov <moorfoinfo@gmail.com>
2023-09-24 18:56:50 +0500

    change list html created

--- moorfoadmin/apps.py
+++ moorfoadmin/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SeatadminConfig(AppConfig):
+class MoorfoAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seatadmin'
+    name = 'moorfoadmin'
---
+++ moorfoadmin/templatetags/moorfo_admin_list.py
@@ -0,0 +1,13 @@
+from django import template
+from django.contrib.admin.templatetags.base import InclusionAdminNode
+register = template.Library()
+
+@register.tag(name="moorfo_admin_change_list_object_tools")
+def change_list_object_tools_tag(parser, token):
+    """Display the row of change list object tools."""
+    return InclusionAdminNode(
+        parser,
+        token,
+        func=lambda context: context,
+        template_name="change_list_object_tools.html",
+    )
